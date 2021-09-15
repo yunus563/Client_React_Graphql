d872064043946dd8fd0555ca5f3f00d1f3e22576
Yunus <59920062+yunus563@users.noreply.github.com>
2021-09-15 18:12:14 +0500

    asd
    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
   return (
     <Grid columns={3}>
       <Grid.Row className="page-title">
-        <h1>Recent Posts</h1>
+        <h1>Foydalanuchilar Posti</h1>
       </Grid.Row>
       <Grid.Row>
         {user && (
